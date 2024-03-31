aa3a0c9fdb701de9fcfbb5c2ea0e2e73908d7460
NF RIDOY <nfridoy@gmail.com>
2024-03-31 15:51:29 +0600

    (mongo setupt)

--- index.js
+++ index.js
@@ -13,6 +13,39 @@ app.use(cors({
     credentials: true
 }));
 
+/**
+ * MongoDB configuration
+ * API endpoints for the server
+ */
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<username>:<password>@cluster"; // example for the DB_URI
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('coders-tasks-server is running');
 })
